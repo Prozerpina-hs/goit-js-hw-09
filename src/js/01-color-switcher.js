360d4dd31e6b691cf78ba3644268a850b87f5b95
Eleni Sydorenkova <Sydorenkova.ev@gmail.com>
2023-05-18 20:34:08 +0300

    1_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,25 +2,25 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
-const startButton = document.querySelector('[data-start]');
-const stopButton = document.querySelector('[data-stop]');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
 let intervalId = null;
 
-startButton.addEventListener('click', () => {
+startBtn.addEventListener('click', () => {
   if (!intervalId) {
-    startButton.disabled = true;
-    stopButton.disabled = false;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
     intervalId = setInterval(() => {
       document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
   }
 });
 
-stopButton.addEventListener('click', () => {
+stopBtn.addEventListener('click', () => {
   if (intervalId) {
-    startButton.disabled = false;
-    stopButton.disabled = true;
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
     clearInterval(intervalId);
     intervalId = null;
   }
